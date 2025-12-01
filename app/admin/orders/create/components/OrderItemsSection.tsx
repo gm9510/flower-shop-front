3b6b4d2fc43239e952d579ff32ca0a4075780a44
gm9510@gmail.com <gm9510@gmail.com>
2025-11-30 21:01:40 -0500

    unused variable

--- app/admin/orders/create/components/OrderItemsSection.tsx
+++ app/admin/orders/create/components/OrderItemsSection.tsx
@@ -42,7 +42,6 @@ export const OrderItemsSection = ({
     setNewItem,
     handleAddItem,
     handleRemoveItem,
-    handleUpdateItemQuantity,
     getProductName,
     formatPrice,
     isSubmitting,
